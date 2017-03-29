b6ceac2e86977024947dea9b93a8d2e7291fa421
ryous0411 <sato_ryo_gn@cyberagent.co.jp>
2017-03-29 10:46:16 +0000

    fix: filePath

--- src/main/scala/jp/co/sample/highcharts/controller/StockViewController.scala
+++ src/main/scala/jp/co/sample/highcharts/controller/StockViewController.scala
@@ -1,25 +1,30 @@
 package jp.co.sample.highcharts.controller
 
+import better.files._
 import io.finch.circe._
 import io.finch.{Endpoint, _}
 import io.circe.generic.auto._
 import jp.co.sample.highcharts.dto.CandleStick
 import jp.co.sample.highcharts.util.{CsvReader, Mt4Converter}
 
+import scala.io.Source
+
 object StockViewController {
+
+  val CSV_DIR = """/home/stock/workspace/stock_server/src/main/resources"""
 
   lazy val all = csvToJson :+: csvToHighchartsData
 
   lazy val csvToJson: Endpoint[Seq[CandleStick]] = post("csvToJson" :: jsonBody[Map[String, String]]) { (map: Map[String, String]) =>
 
-    val resoucePath = getClass.getResource(s"/${map("fileName")}");
-    Ok(CsvReader.read(resoucePath.getPath))
+    val filePath = file"$CSV_DIR/${map("fileName")}"
+    Ok(CsvReader.read(filePath.path.toString))
   }
 
   lazy val csvToHighchartsData: Endpoint[Seq[Seq[BigDecimal]]] = post("csvToHighchartsOHLC" :: jsonBody[Map[String, String]]) { (map: Map[String, String]) =>
 
-    val resoucePath = getClass.getResource(s"/${map("fileName")}");
-    val ohlcResults: Seq[Seq[BigDecimal]] = CsvReader.read(resoucePath.getPath)
+    val filePath = file"$CSV_DIR/${map("fileName")}"
+    val ohlcResults: Seq[Seq[BigDecimal]] = CsvReader.read(filePath.path.toString)
       .map(candleStick => Seq(
         BigDecimal(Mt4Converter.toTimeInMilles(candleStick.date, candleStick.time)),
         BigDecimal(candleStick.open),
